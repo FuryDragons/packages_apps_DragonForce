f9b740968111c4589c8211a420841872e8573dec
Nyks45 <winglow45@gmail.com>
2016-11-23 20:29:14 +0300

    Squash CMPARTS Convert

---
+++ src/dragons/nyks45/furyforce/AboutDragons.java
@@ -0,0 +1,40 @@
+package dragons.nyks45.furyforce;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.support.v4.app.Fragment;
+
+public class AboutDragons extends Fragment {
+   
+   @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().getFragmentManager().beginTransaction()
+                .replace(R.id.content_dragon, new SettingsPreferenceFragment())
+                .commit();
+    }
+
+    public static class SettingsPreferenceFragment extends PreferenceFragment {
+
+        public SettingsPreferenceFragment() {
+        }		
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.furydragons_about);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+            Activity activity = getActivity();
+
+            final ContentResolver resolver = getActivity().getContentResolver();
+
+        }
+    }
+}
--- src/dragons/nyks45/furyforce/DragonActivity.java
+++ src/dragons/nyks45/furyforce/DragonActivity.java
@@ -1,20 +1,25 @@
 package dragons.nyks45.furyforce;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class DragonActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String NAV_ITEM_ID = "navItemId";
+
+    private DrawerLayout mDrawer;
+    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +28,14 @@ public class DragonActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction replace = tx.replace(R.id.content_dragon, new AboutDragons());
+        tx.commit();
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -43,19 +52,53 @@ public class DragonActivity extends AppCompatActivity
         }
     }
 
-
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        // update highlighted item in the navigation menu
+        item.setChecked(true);
+        id = item.getItemId();
+        Fragment fragment = null;
 
-        if (id == R.id.nav_about) {
+        Class fragmentClass;
 
+        switch (id) {
+            case R.id.nav_about:
+                fragmentClass = AboutDragons.class;
+                break;
+				case R.id.nav_about_us:
+                fragmentClass = FuryDragonsAboutUs.class;
+                break;
+            default:
+                fragmentClass = AboutDragons.class;
         }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        RelativeLayout contentMain = (RelativeLayout) findViewById(R.id.content_dragon);
+        contentMain.removeAllViewsInLayout();
+        contentMain.invalidate();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_dragon, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        item.setChecked(true);
+        setTitle(item.getTitle());
+        mDrawer.closeDrawers();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ITEM_ID, id);
     }
 }
